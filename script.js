fe53d4b87c8fdda7d555d4e292ccbeb2513dd060
gusty <gustavodiorlando@gmail.com>
2023-07-07 12:01:21 -0300

    More styles + password match script

---
+++ script.js
@@ -0,0 +1,28 @@
+const pass = document.querySelector('input#password');
+const passConfirm = document.querySelector('input#password-confirm');
+const passInputs = document.querySelectorAll("input[type='password']");
+const createButton = document.querySelector("button[type='submit']");
+const notMatch = document.querySelector('div.not-match');
+
+createButton.addEventListener('click', (e) => {
+    if (!(pass.value === passConfirm.value)) {
+        e.preventDefault();
+        notMatch.textContent = '* Passwords do not match.';
+        passInputs.forEach((input) => {
+            input.style = 'border-color: rgb(116, 0, 29);';
+        })
+    } else {
+        notMatch.textContent = '';
+    }
+});
+
+passInputs.forEach((input) => {
+    input.addEventListener('change', () => {
+        if (pass.value === passConfirm.value) {
+            notMatch.textContent = '';
+            passInputs.forEach((input) => {
+                input.style = 'border-color: rgb(53, 53, 53);'
+            })
+        }
+    })
+})
